0c5486e45ff0124b98ce844d45c870e2d3633122
Cardiacss <filberthvalentino@gmail.com>
2024-10-02 17:44:33 +0700

    Initial commit

---
+++ SampleSecureWeb/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SampleSecureWeb.Models;
+
+namespace SampleSecureWeb.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+        public IActionResult About()
+    {
+        ViewData["Title"] = "About Page";
+        ViewBag.username="filbert";
+        string[] fruits = new string[]{"Apple","Banana","Orange"};
+        ViewBag.fruits = fruits;
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
