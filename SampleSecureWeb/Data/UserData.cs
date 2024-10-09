e83c86050776a5f8af2225264d35c916294ffdb0
FILBERT <112719917+Cardiacss@users.noreply.github.com>
2024-10-09 19:38:03 +0700

    Add files via upload

---
+++ SampleSecureWeb/Data/UserData.cs
@@ -0,0 +1,45 @@
+using System;
+using SampleSecureWeb.Models;
+using SQLitePCL;
+using bc = BCrypt.Net.BCrypt;
+
+namespace SampleSecureWeb.Data;
+
+public class UserData : IUser
+{
+    private readonly ApplicationDbContexts _db;
+
+    public UserData(ApplicationDbContexts db)
+    {
+        _db = db;
+    }
+    public User login(User user)
+    {
+        var _user = _db.Users.FirstOrDefault(u => u.Username == user.Username);
+        if (_user == null)
+        {
+            throw  new Exception("User Not Found");
+        }
+        if(!BCrypt.Net.BCrypt.Verify(user.Password, _user.Password))
+        {
+            throw new Exception("Password Is Incorrect");
+        }
+        return _user;
+    }
+
+    public User registration(User user)
+    {
+        try
+        {
+            user.Password = BCrypt.Net.BCrypt.HashPassword  (user.Password);
+            _db.Users.Add(user);
+            _db.SaveChanges();
+            return user;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+}
+
